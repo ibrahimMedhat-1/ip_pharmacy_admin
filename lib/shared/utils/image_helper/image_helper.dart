47bb93ca2603e1e1e0106c3228d97406c64e86e4
Ibrahim Medhat <ibrahimmedhat112@gmail.com>
2024-12-30 13:44:12 +0200

    agp

--- lib/shared/utils/image_helper/image_helper.dart
+++ lib/shared/utils/image_helper/image_helper.dart
@@ -30,8 +30,10 @@ class ImageHelper {
     required XFile file,
     CropStyle cropStyle = CropStyle.rectangle,
   }) async =>
-      await _imageCropper.cropImage(sourcePath: file.path, cropStyle: cropStyle, uiSettings: [
+      await _imageCropper.cropImage(sourcePath: file.path,  uiSettings: [
+
         AndroidUiSettings(
+          cropStyle: cropStyle,
           initAspectRatio: CropAspectRatioPreset.original,
         )
       ]);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,9 +10,11 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
+import flutter_inappwebview_macos
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -21,6 +23,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
